3bbde783ec284de72a776a959aca517cb4098b4c
Adrian72808 <168490652+Adrian72808@users.noreply.github.com>
2024-05-25 00:41:09 +0200

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Zadanie5_Lab3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            Pen pen = new Pen(Color.Blue, 2);
+
+            int width = this.ClientSize.Width;
+            int height = this.ClientSize.Height;
+            int amplitude = height / 4;
+            int offsetX = 0;
+            int offsetY = height / 2;
+
+            // Tabela punktów dla sinusoidy
+            PointF[] points = new PointF[width];
+
+            for (int x = 0; x < width; x++)
+            {
+                //Pbliczanie wartości y dla x przy pomocy fukcji sisus
+                float y = (float)(amplitude * Math.Sin((x + offsetX) * 2 * Math.PI / width) + offsetY);
+                points[x] = new PointF(x, y);
+            }
+            // Rysowanie sinusoidy 
+            g.DrawLines(pen, points);
+        }
+    }
+}
